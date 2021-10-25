da7feb35606cb765acdc76ff19d4d3c9cbff12f9
Vanderllee <vanderllee@gmail.com>
2021-10-25 16:18:31 -0300

    added Home screen and Header Component

--- App.tsx
+++ App.tsx
@@ -1,21 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Home } from './src/screens/Home';
+
+
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold
+} from '@expo-google-fonts/roboto';
+
+import AppLoading from "expo-app-loading";
+import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    Roboto_700Bold,
+    Roboto_400Regular
+  })
+
+  if (!fontsLoaded) {
+    <AppLoading />
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar style="light" />
+      <Home />
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+
+import { COLORS, FONTS } from '../../theme';
+
+export const styles = StyleSheet.create({
+
+    container: {
+        flex:1,
+        backgroundColor: COLORS.BLACK_SECONDARY,
+        
+    }
+
+})
